822c22809a81c651c7919d7256536242e52e56fd
howtomakeaturn <howtomakeaturn@hotmail.com>
2018-11-15 14:08:53 +0800

    update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -18,12 +18,12 @@ class Controller extends BaseController
     {
         set_time_limit(0);
 
-        for ($i=0; $i <100 ; $i++) {
+        for ($i=0; $i <800 ; $i++) {
             DB::transaction(function() {
                 $this->_generate();
              });
         }
-
+        return '';
         return view('welcome');
     }
 
